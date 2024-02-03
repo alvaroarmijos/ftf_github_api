adb5e0527435bdecefd7079d4e31fbcece86ad8e
Alvaro Armijos <alvaro.armijoss@gmail.com>
2024-02-03 00:00:46 -0500

    Create Home feature and empty bloc.

---
+++ lib/src/packages/features/home/src/bloc/home_state.dart
@@ -0,0 +1,20 @@
+part of 'home_bloc.dart';
+
+sealed class HomeState extends Equatable {
+  const HomeState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class HomeLoading extends HomeState {
+  const HomeLoading();
+}
+
+final class HomeSuccess extends HomeState {
+  const HomeSuccess();
+}
+
+final class HomeError extends HomeState {
+  const HomeError();
+}
